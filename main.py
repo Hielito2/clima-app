4918eb0d0317c5954fa20c7bd0fde1ad970fb29d
Hielito <36553765+Hielito2@users.noreply.github.com>
2026-04-11 05:12:11 -0600

    Procesa datos de la API y prepara visualización en terminal
    Se extraen los campos principales.
    Se plantea usar matplotlib como opcional (requiere instalación aparte).

--- main.py
+++ main.py
@@ -17,14 +17,6 @@ def get_coords(api, city):
     return cords2
 
 
-def get_weather(api, coords):
-    url = f"https://api.openweathermap.org/data/2.5/weather?{coords}&lang=es&unit=standard&appid={api}"
-    data_request = requests.get(url)
-    content = data_request.content.decode()
-    print(content)
-    plop = json.loads(content)
-    print(plop['coord'])
-
 
 def get_weather2(api, coords, city):
     url = f"https://api.openweathermap.org/data/2.5/forecast?{coords}&units=metric&appid={api}"
@@ -45,7 +37,24 @@ def get_weather3(api, coords, city):
     #place = content['name']
     #country = content['country']
 
-    print(content)
+    return json.loads(content)
+
+
+def print_data(data):
+    # For get_weather3
+    print("Country: ", data['sys']['country'])
+    print("Name: ", data['name'])
+    print("Weather: ", data['weather'][0]['main'], data['weather'][0]['description'])
+
+    print("Temperature: ", data['main']['temp'], '°C', f' (Feels like: {data['main']['feels_like']}°C)')
+    print("Temperature Max: ", data['main']['temp_max'], '°C')
+    print("Temperature Min: ", data['main']['temp_min'], '°C')
+    print("Pressure: ", data['main']['pressure'], 'hPa')
+    print("Humidity: ", data['main']['humidity'], '%')
+    print("Visibility: ", float(data['visibility']) / 100, 'km')
+
+    # For 
+    print(data)
 
 
 
@@ -53,6 +62,8 @@ def get_weather3(api, coords, city):
 city = "4014338"
 api = read_config()
 coords = get_coords(api, city)
+get_weather2(api, coords, city)
 
-weather = get_weather(api, coords)
-#weathe2 = get_weather2(api, coords, city)
+
+#weathe2 = get_weather3(api, coords, city)
+#print_data(weathe2)
